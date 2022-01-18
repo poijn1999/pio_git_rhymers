8603bd56190872b73591b5347c252db3539f0b24
Maciej Chodakowski <228708@edu.p.lodz.pl>
2022-01-18 22:56:03 +0100

    1.4 accessory 1

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
 	private int[] numbers = new int[12];
 
-	public int total = -1;
+	private int total = -1; //1
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -19,7 +19,7 @@ public class DefaultCountingOutRhymer {
 		return total == 11;
 	}
 
-	protected int peekaboo() {
+	int peekaboo() { //2
 		if (callCheck())
 			return -1;
 		return numbers[total];
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery.list;
 
-public class IntLinkedList {
+class IntLinkedList { //5
 
-	Node last;
+	private Node last; //6
 	int i;
 
 	public void push(int i) {
@@ -15,7 +15,7 @@ public class IntLinkedList {
 		}
 	}
 
-	public boolean isEmpty() {
+	private boolean isEmpty() {//7
 		return last == null;
 	}
 

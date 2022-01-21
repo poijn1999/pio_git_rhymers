14f72c0e80fe30d18c57fc5696b54ccf4cd49b20
Maciej Chodakowski <228708@edu.p.lodz.pl>
2022-01-21 13:14:35 +0100

    1.5.6 Generowanie automatycznego szkieletu dokumentacji do klasy

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**default counting out rhymer
+ * @author Maciek
+ * Klasa służy do grania w gry wyliczankowe
+ */
+
 public class DefaultCountingOutRhymer {
 
 	private static final int FULL = 11;
@@ -11,26 +16,44 @@ public class DefaultCountingOutRhymer {
 	private final int[] numbers = new int[SIZE];
 
 	public int total = COUNTER;
-
+	/**countIN
+	 * @author Maciek
+	 *Funkcja sprawdza czy tablica jest pełna, jezeli nie, to wpisuje wartość IN do tablicy
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * @author Maciek
+	 * Funkcja wywołuje sprawdzenie tablicy
+	 */
 	public boolean callCheck() {
 		return total == COUNTER;
 	}
 
+	/**
+	 * @author Maciek
+	 * Funkcja sprawdza czy tablica jest pełna
+	 */
 	public boolean isFull() {
 		return total == FULL;
 	}
 
+	/**
+	 * @author Maciek
+	 * Funkcja zwraca elementy z tablicy
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return COUNTER;
 		return numbers[total];
 	}
-
+	/**
+	 * @author Maciek
+	 * Funkcja zwraca poprzedni element tablicy
+	 */
 	public int countOut() {
 		if (callCheck())
 			return COUNTER;
